44e6661a08e79dab14e478400b20b53744452e21
LaurelOlson <laurel.olson@outlook.com>
2016-02-22 19:31:26 +0000

    added song model plus validations for presence of title/artist and format of url

---
+++ app/models/song.rb
@@ -0,0 +1,13 @@
+class Song < ActieRecord::Base
+
+  validates :title,
+    presence: true
+
+  validates :artist,
+    presence: true
+
+  # regex matches HTTP/HTTPS URL
+  validates :url,
+    format: { with: /(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)|\A\Z|)/ }
+
+end
