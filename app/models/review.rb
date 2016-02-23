299eea8b041cc6fb5723c0f3b52d830c9d4aee57
LaurelOlson <laurel.olson@outlook.com>
2016-02-23 23:17:45 +0000

    implemented star rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,8 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :song
 
+  validates :rating,
+    presence: true,
+    numericality: { greater_than: 0, less_than_or_equal_to: 5 }
+
 end
